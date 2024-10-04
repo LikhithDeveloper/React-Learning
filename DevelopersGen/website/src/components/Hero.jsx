d6c4317bef86a6560c68f61ee18debbf94dd6dfe
Likhith Panigrahi <LikhithDeveloper>
2024-10-04 21:48:10 +0530

    Hero page 70% completed

--- DevelopersGen/website/src/components/Hero.jsx
+++ DevelopersGen/website/src/components/Hero.jsx
@@ -1,7 +1,33 @@
-import "../App.css"
+import "./Hero.css";
 
 const Hero = () => {
-    return <div className="hero">x</div>
-}
+  return (
+    <section className="hero">
+      <div className="hero-1">
+        <a href="" className="linkedin">
+          LinkedIn
+        </a>
+        <a href="" className="insta">
+          Instagram
+        </a>
+        <a href="" className="mail">
+          EMail
+        </a>
+      </div>
+      <div className="hero-2">
+        <h1>Creative & Minimal</h1>
+        <h2>IT AGENCY.<span className="it"></span></h2>
+        <p>
+          We provide tailored IT solutions that elevate your business,
+          streamline operations, and enhance user experience. Our comprehensive
+          approach encompasses web development, app design, UI/UX design, and
+          strategic consulting to ensure your success.
+        </p>
+        <button className="hero-2-btn">Contact Us<span className="hero-2-btn-span"></span></button>
+      </div>
+      <div className="hero-3">z</div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
